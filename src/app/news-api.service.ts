231902fe56bef197e10a6d175190b4f4e6c510b3
activevj <uservjkumar@gmail.com>
2019-12-07 12:13:08 +0530

    1 file added

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn :'root'
+})
+export class NewsApiService {
+  api_key='240c2de79c2b44ceac8dfeeb7c9a388f';
+
+  construtor(private http:HttpClient){}
+  
+  initSources(){
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey='+this.api_key);
+  }
+  initArticles(){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key);
+  }
+  getArticlesById(source: String){
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key);
+  }
+}
