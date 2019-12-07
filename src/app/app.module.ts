1c384adca3619a6d582ab8f2bb308f5cfde2909d
activevj <uservjkumar@gmail.com>
2019-12-07 11:50:21 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsApiService } from './news-api.service';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule,
+   FormsModule,
+   BrowserAnimationsModule,
+   HttpClientModule,
+   MatButtonModule,
+   MatMenuModule,
+   MatCardModule,
+   MatToolbarModule,
+   MatIconModule,
+   MatSidenavModule,
+   MatListModule,
+   ],
+  providers: [NewsApiService],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
